5614ef091b42ada4c9e73693a53807bde782066b
Lukasz97947 <91849@student.pb.edu.pl>
2026-01-03 15:34:48 +0100

    Dodano klasę Notatka dziedziczącą po Wpis

---
+++ Projekt/Notatka-Manager.cs
@@ -0,0 +1,54 @@
+using System.Formats.Asn1;
+using System.Runtime.CompilerServices;
+
+public partial class Program
+{
+    // Klasa Notatka dziedziczy po klasie abstrakcyjnej Wpis
+    // Reprezentuje pojedyncz¹ notatkê w systemie.
+    public class Notatka : Wpis
+    {
+
+
+        public bool Ulubiona { get; private set; }// Okreœla, czy notatka jest oznaczona jako ulubiona
+        public List<Tag> Tagi { get; set; }// Lista tagów przypisanych do notatki
+
+
+        public Notatka(string tytul, string tresc, List<Tag> tagi)// Konstruktor klasy Notatka
+            : base(tytul, tresc)
+        {
+
+            if (tagi != null)
+                Tagi = tagi;
+            else
+                Tagi = new List<Tag>();
+
+            Ulubiona = false; // domyœlnie notatka nie jest ulubiona
+        }
+
+        // Ustawia lub usuwa status ulubionej notatki
+        public void UstawUlubione(bool ustawienie)
+        {
+            Ulubiona = ustawienie;
+        }
+
+        // Nadpisanie metody abstrakcyjnej WypiszInformacje() z klasy Wpis
+        // Zwraca wszystkie informacje o notatce w formie stringa
+        public override string WypiszInformacje()
+        {
+            string tagiStr;
+
+            if (Tagi.Count > 0)
+                tagiStr = string.Join(", ", Tagi.Select(t => t.Nazwa));
+            else
+                tagiStr = "Brak tagów";
+
+            return $"ID: {ID}, Tytu³: {Tytul}, Treœæ: {Tresc}, Ulubiona: {Ulubiona}, Tagi: {tagiStr}";
+        }
+
+        // Nadpisanie ToString() dla wygodnego wypisywania notatki
+        public override string ToString()
+        {
+            return WypiszInformacje();
+        }
+    }
+}
